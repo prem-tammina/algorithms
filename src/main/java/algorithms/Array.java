49c8d362e3fef2e550e769ba7956268a7ed06f6b
PremTammina <premtammina@gmail.com>
2019-09-06 08:30:24 +0530

    array basic added

---
+++ src/main/java/algorithms/Array.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+public class Array {
+	public static void main(String[] args) {
+		int[] num = new int[5];
+
+		for (int i = 0; i < num.length; i++) {
+			num[i] = i;
+		}
+		int number = num[3];
+		System.out.println(number);
+
+	}
+}
